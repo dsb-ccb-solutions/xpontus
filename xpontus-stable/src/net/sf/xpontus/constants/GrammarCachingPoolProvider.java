dd556cc8ebc9924b0778179887c7922272be097b
yveszoundi <yveszoundi@5acf1fb6-5d16-0410-9810-d74fc68da232>
2007-02-16 02:13:59 +0000

    license fixes

--- xpontus-stable/src/net/sf/xpontus/constants/GrammarCachingPoolProvider.java
+++ xpontus-stable/src/net/sf/xpontus/constants/GrammarCachingPoolProvider.java
@@ -3,20 +3,30 @@
  *
  * Created on February 15, 2007, 7:15 PM
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ *  Copyright (C) 2005 Yves Zoundi
+ *
+ *  This library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation; either version 2.1 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-
 package net.sf.xpontus.constants;
 
  import org.apache.xerces.util.SymbolTable;
 import org.apache.xerces.util.*;
-import org.apache.xerces.xni.grammars.Grammar;
-import org.apache.xerces.xni.grammars.XMLGrammarDescription;
 import org.apache.xerces.xni.grammars.XMLGrammarPool;
 
 /**
- *
+ * A class which provides grammar caching
  * @author Yves Zoundi
  */
 public class GrammarCachingPoolProvider {
--- xpontus-stable/src/net/sf/xpontus/utils/EncodingHelper.java
+++ xpontus-stable/src/net/sf/xpontus/utils/EncodingHelper.java
@@ -1,10 +1,23 @@
 /*
  * EncodingHelper.java
  *
- * Created on February 15, 2007, 8:04 PM
+ * Created on February 15, 2007, 8:04 PM 
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ *  Copyright (C) 2005 Yves Zoundi
+ *
+ *  This library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation; either version 2.1 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
 package net.sf.xpontus.utils;
