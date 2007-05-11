41db616634a892d99d206d317ed6b92a487bf17b
yveszoundi <yveszoundi@5acf1fb6-5d16-0410-9810-d74fc68da232>
2007-05-11 22:08:18 +0000

    ""

--- xpontus-stable/src/net/sf/xpontus/controller/actions/ReloadAction.java
+++ xpontus-stable/src/net/sf/xpontus/controller/actions/ReloadAction.java
@@ -3,10 +3,23 @@
  *
  * Created on April 10, 2007, 7:30 PM
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ *
+ *  Copyright (C) 2005-2007 Yves Zoundi
+ *
+ *  This library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation; either version 2.1 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-
 package net.sf.xpontus.controller.actions;
 
 import com.ibm.icu.text.CharsetDetector;
@@ -36,8 +49,8 @@ import org.syntax.jedit.SyntaxDocument;
 import org.syntax.jedit.tokenmarker.TokenMarker;
 
 /**
- *
- * @author mrcheeks
+ * A class to reload the opened document
+ * @author Yves Zoundi
  */
 public class ReloadAction extends BaseAction{
     
