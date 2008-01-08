210dddab5374710c967f7b45eb8d2fc380aae7f1
yveszoundi <yveszoundi@5acf1fb6-5d16-0410-9810-d74fc68da232>
2008-01-08 00:11:49 +0000

    XML security support is almost here

---
+++ xpontus/xmlsecurity/src/main/java/net/sf/xpontus/plugins/xmlsecurity/SignDocumentAction.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.sf.xpontus.plugins.xmlsecurity;
+
+import net.sf.xpontus.actions.impl.XPontusThreadedActionImpl;
+
+/**
+ *
+ * @author Propriétaire
+ */
+public class SignDocumentAction extends XPontusThreadedActionImpl {
+
+    public SignDocumentAction() {
+        
+        
+        
+    }
+
+    
+    
+    
+    public void run() {
+         
+    }
+
+}
