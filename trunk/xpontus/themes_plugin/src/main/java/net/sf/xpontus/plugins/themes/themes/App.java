08ab76790cd3c70fe41a2b0881503dadc4cacc72
yveszoundi <yveszoundi@5acf1fb6-5d16-0410-9810-d74fc68da232>
2008-03-05 02:57:29 +0000

    Update files

--- trunk/xpontus/themes_plugin/src/main/java/net/sf/xpontus/plugins/themes/themes/App.java
+++ trunk/xpontus/themes_plugin/src/main/java/net/sf/xpontus/plugins/themes/themes/App.java
@@ -1,13 +1,12 @@
 package net.sf.xpontus.plugins.themes.themes;
 
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
